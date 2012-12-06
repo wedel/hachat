2c65f2afb0d22df1f7d51b743d8959f5e38d402c
t2d <t@tecra.(none)>
2012-12-06 12:50:57 +0100

    argparse und todo

--- hachat.py
+++ hachat.py
@@ -1,10 +1,20 @@
 # coding = utf-8
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-import sys
+import argparse
+import re
 from peer import Peer
 
-if len(sys.argv) == 3:
-    peer = Peer(firstHost = (sys.argv[1], sys.argv[2]))
+parser = argparse.ArgumentParser(description="start peer with arguments")
+parser.add_argument("-p")
+parser.add_argument("-l")
+args = parser.parse_args()
+
+print args
+print args.l
+
+if args.l != None:
+    (hostIP, hostPort) = re.split(":", args.l, 1)
+    peer = Peer(firstHost = (hostIP, hostPort))
 else:
     peer = Peer()
