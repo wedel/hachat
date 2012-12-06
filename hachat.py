e73ed156a60fc1e9cd8cfbdf435271f4ab375d51
t2d <t@tecra.(none)>
2012-12-06 13:56:51 +0100

    set name as argument or generate random one

--- hachat.py
+++ hachat.py
@@ -3,15 +3,23 @@
 
 import argparse
 import re
+import random
 from peer import Peer
 
 parser = argparse.ArgumentParser(description="start peer with arguments")
 parser.add_argument("-p", help="Port to listen on")
 parser.add_argument("-l", help="hachat link (ex. localhost:12345)")
+parser.add_argument("-n", help="set own name")
 args = parser.parse_args()
+
+# set name
+name = args.n
+if name == None:
+    nr = random.randint(0, 99999)
+    name = "user" + str(nr)
 
 if args.l != None:
     (hostIP, hostPort) = re.split(":", args.l, 1)
-    peer = Peer(firstHost = (hostIP, hostPort), port = args.p)
+    peer = Peer(firstHost = (hostIP, hostPort), port = args.p, name = name)
 else:
-    peer = Peer(port = args.p)
+    peer = Peer(port = args.p, name = name)
--- peer.py
+++ peer.py
@@ -18,9 +18,13 @@ class Peer:
     ip = None # IP des Peers
     port = None # Port auf dem der Peer hört
     hosts = {} # Dict. der bekannten Hosts
-    name = "temp"
+    name = None
 
-    def __init__(self, firstHost = None, port = None):
+    def __init__(self, firstHost = None, port = None, name = "temp"):
+        
+        self.name = name # set peer name
+        
+        # open socket
         self.inSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         if port == None:
             # bind on random port
@@ -32,8 +36,8 @@ class Peer:
         self.port = int(self.inSocket.getsockname()[1])
         print "Listening on port", self.port
         
+        # send HELO to first host if you know one
         if firstHost != None:
-            # send HELO to first host if you know one
             (hostIP, hostPort) = firstHost
             h = Host(self, hostIP, hostPort)
             h.sendHello()
@@ -83,7 +87,7 @@ class Peer:
         key = hostIP + ':' + str(hostPort) # construct key
         
         if key in self.hosts:
-            print key, "alread in hostlist"
+            print key, "already in hostlist"
         else:
             #insert in host dict
             print "adding", key, "to hostlist" 
