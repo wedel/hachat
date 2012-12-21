29849d810604b20fa42c3c614af3416b87550b23
_tnull <trash@tnull.de>
2012-12-21 16:04:32 +0100

    Der arme Arthur atmet wieder...

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ port1=5001
 port2=5002
 
 (python hachat.py -i $address -p $port1 -n Anna -v >> Anna.log 2>&1 &)
-(python hachat.py -l ${address}:$Port1 -n Arthur -v >> Arthur.log 2>&1 &)
+(python hachat.py -l ${address}:$port1 -n Arthur -v >> Arthur.log 2>&1 &)
 (python hachat.py -i $address -p $port2 -l localhost:$port1 -n Paul -v >> Paul.log 2>&1 &)
 (python hachat.py -l ${address}:$port2 -n Pauline -v >> Pauline.log 2>&1 &)
 (python hachat.py -l ${address}:$port2 -n Zora -v >> Zora.log 2>&1 &)
