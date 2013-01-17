422204d8537be804a807ecbfcc2b15e85d08e98d
wedel <wedel@informatik.hu-berlin.de>
2013-01-17 16:22:36 +0100

    changes to test.sh and readme

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 hachat
 ======
-
-Projekt Peer-to-Peer Systeme 
+A peer-to-peer-network, designed for "Peer-to-Peer Systeme", Humboldt-Universität Berlin. 
 
 test
 ----
-python hachat.py
+You can use hachat.py or test.sh to test the network.
+Usage: ./hachat.py [-l Link] [-i IP-Adress] [-p Port] [-n name] [-v verbose] [-T Test-Mode]
+A link or your own ip adress is nessesery to start HaChat. The Link must be a hostname:port pair.
 
-python hachat.py [-l Link] [-p Port] [-n name] [-v verbose]
-    
-Link must be a hostname:port pair
+You can use test.sh to start 5 clients on localhost. Pass over a time (in sec.) and starting the first three and the last two peers will halt. This way you can test the history-functionality.
+Usage: ./test.sh [time in seconds]
+
+Have fun chatting...
--- test.sh
+++ test.sh
@@ -7,20 +7,19 @@ port1=5001
 port2=5002
 if echo $1 | egrep -q '^[0-9]+$'; then
     # $1 is a number
+    sec=$1
     echo "HaChat0.1: Will start 5 Peers for localhost..."
     echo "We will wait for $1 second(s) between starting first 3 and last 2 peers"
     echo "have fun chatting..."
 else
-    echo "need to get number as first argument for sleeping time between starting first 3 and last 2 peers"
-    echo "exiting..."
-    exit 0
+   sec=0
 fi
 
 
 (python hachat.py -i $address -p $port1 -n Anna -v >> Anna.log 2>&1 &)
 (python hachat.py -l ${address}:$port1 -n Arthur -v >> Arthur.log 2>&1 &)
 (python hachat.py -i $address -p $port2 -l localhost:$port1 -n Paul -v >> Paul.log 2>&1 &)
-sleep $1
+sleep $sec
 (python hachat.py -l ${address}:$port2 -n Pauline -v >> Pauline.log 2>&1 &)
 (python hachat.py -l ${address}:$port2 -n Zora -v >> Zora.log 2>&1 &)
 
